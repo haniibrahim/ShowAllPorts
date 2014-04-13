bafb0b957fff983b0b31d59bf4b6e337aaa2c4a1
Hani Ibrahim <hani.ibrahim@gmx.de>
2014-04-13 23:29:58 +0200

    Initial commit

---
+++ src/de/hani_ibrahim/showallports/MacImpl.java
@@ -0,0 +1,65 @@
+package de.hani_ibrahim.showallports;
+
+import com.apple.eawt.AboutHandler;
+import com.apple.eawt.AppEvent.AboutEvent;
+import com.apple.eawt.AppEvent.PreferencesEvent;
+import com.apple.eawt.AppEvent.QuitEvent;
+import com.apple.eawt.Application;
+import com.apple.eawt.PreferencesHandler;
+import com.apple.eawt.QuitHandler;
+import com.apple.eawt.QuitResponse;
+import javax.swing.ImageIcon;
+
+
+/**
+ *
+ * @author Hani Ibrahim
+ */
+class MacImpl implements ClassSelector, AboutHandler, PreferencesHandler, QuitHandler {
+
+    Application application;
+
+    public MacImpl() {
+        handleOS();
+    }
+
+    public void handleOS() {
+
+        try {
+            application = Application.getApplication();
+            application.setAboutHandler(this);
+            // PreferenceHandler not used yet
+            // application.setPreferencesHandler(this); 
+        } catch (Throwable e) {
+            System.err.println("setupMacOSXApplicationListener failed: "
+                    + e.getMessage());
+        }
+        
+        // Set dock icon
+        application.setDockIconImage(new ImageIcon(getClass().getResource("interface.png")).getImage());
+
+    }
+
+    public void handleAbout(AboutEvent arg0) {
+        InfoDialog info = new InfoDialog(ShowAllPorts.getFrames()[0], true);
+//        info.setLocationRelativeTo(null);
+        info.setLocationRelativeTo(null /*ShowAllPorts.getFrames()[0]*/);
+        info.setVisible(true);
+        // System.out.println("handleAbout()"); 
+    }
+
+    // not used yet
+    public void handlePreferences(PreferencesEvent arg0) {
+        // new OptionsDialog(); 
+        //System.out.println("handlePreferences()"); 
+    }
+
+    public void handleQuitRequestWith(QuitEvent arg0, QuitResponse arg1) {
+        System.exit(0);
+    }
+}
+
+interface ClassSelector {
+
+    public void handleOS();
+}
