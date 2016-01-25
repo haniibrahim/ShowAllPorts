62d679d0eaf578d2fbc3ba933f2d40f1fd0cde96
Hani Andreas Ibrahim <hani.ibrahim@gmx.de>
2016-01-25 01:09:47 +0100

    Clean GIT repo

---
+++ test/de/hani_ibrahim/showallports/InfoDialogTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.hani_ibrahim.showallports;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hi
+ */
+public class InfoDialogTest {
+    
+    public InfoDialogTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
